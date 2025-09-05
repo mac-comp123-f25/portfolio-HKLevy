2d8d9c5d54614815bce7194a064f8b0307900d17
HKLevy <hlevy@macalester.edu>
2025-09-05 10:34:31 -0500

    fixed bugs in the ica file scripts a-e.
    fixed bugs in the ica file scripts a-e.

--- src/ica/01-python+ide/buggy_scripts/script_c.py
+++ src/ica/01-python+ide/buggy_scripts/script_c.py
@@ -1,6 +1,6 @@
 # This program converts celsius to fahrenheit
 
 cels_temp = 30
-fraction = 5/9
+fraction = 9/5 #changed this from 5/9 to 9/5 because 5/9 is wrong
 fahr_temp = (cels_temp * fraction) + 32
 print(fahr_temp)
