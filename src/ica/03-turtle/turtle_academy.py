2659aa6f17921a30738151b220752cd5ad1371e3
HKLevy <hlevy@macalester.edu>
2025-09-12 13:13:57 -0500

    ica for sept 12 done.

--- src/ica/03-turtle/turtle_academy.py
+++ src/ica/03-turtle/turtle_academy.py
@@ -1,6 +1,7 @@
 '''
-Draw the circle, filled in yellow.
-Then draw the triangles, filled in orange.
+Draws a sun-like shape.
+
+Worked on by Gabby, Apui, Helen
 '''
 
 import turtle
@@ -8,23 +9,31 @@ wn = turtle.Screen()
 t = turtle.Turtle()
 
 #creating the circle
+t.penup()
+t.goto(0,-100) #ensuring that the center of the circle is the middle of the screen
 t.fillcolor('yellow')
 t.begin_fill()
-t.penup()
 t.circle(100)
 t.end_fill()
 
 t.right(90)
 t.forward(50)
 t.left(90)
+
 #creating the triangles
-t.pendown() #will delete later, keeping in to see where the turtle is
+'''We decided to simplify the problem a bit
+by drawing equilateral triangles rather than
+isosceles triangles'''
+
+t.fillcolor('orange')
 for triangle in range(12):
-    t.left(30) #this is wrong. fix later
+    t.left(60)
+    t.begin_fill()
     for side in range(3):
         t.forward(30)
         t.left(120)
-    t.right(30)
+    t.end_fill()
+    t.right(60)
     t.circle(150,30)
 
 wn.exitonclick()
