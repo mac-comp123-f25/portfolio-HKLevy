71bcc6e249453b11403c871dbd0a5e50a7b1c89a
HKLevy <hlevy@macalester.edu>
2025-09-19 20:46:28 -0500

    finished connected n-gon

--- src/Practice Problems/Turtle Academy/connected_n-gon.py
+++ src/Practice Problems/Turtle Academy/connected_n-gon.py
@@ -1,15 +1,35 @@
 import turtle
+import math
 wn = turtle.Screen()
 t = turtle.Turtle()
+t.speed(0)
+t.dot(5)
+
+def goto_corner(n,side_length):
+    tip_angle = 360/n
+    twin_angle = (180-tip_angle)/2
+    dist = side_length*math.sin(math.radians(twin_angle))/math.sin(math.radians(tip_angle))
+    t.up()
+    t.goto(0,0)
+    t.seth(0)
+    t.rt(90+tip_angle/2)
+    t.fd(dist)
+    t.lt(90+tip_angle/2)
+
 def get_points(n,side_length):
-    #figure out how to center this
     point_list=[]
-    angle = 180-(180*(n-2)/n)
+    angle = 360/n
+    goto_corner(n,side_length)
     for x in range(n):
+        point_list.append(t.pos())
         t.fd(side_length)
         t.lt(angle)
-        t.dot(5)
-        point_list.append(t.pos())
     return point_list
-get_points(8,100)
+
+my_points=get_points(24,75)
+t.down()
+for x in range(len(my_points)):
+    for y in range(x,len(my_points)):
+        t.goto(my_points[x])
+        t.goto(my_points[y])
 wn.exitonclick()
