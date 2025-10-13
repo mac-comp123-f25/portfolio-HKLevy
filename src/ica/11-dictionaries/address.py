2c95836aa6125f053674a84d8e947842c809852c
HKLevy <hlevy@macalester.edu>
2025-10-13 10:15:37 -0500

    Create address.py

---
+++ src/ica/11-dictionaries/address.py
@@ -0,0 +1,42 @@
+betsy_info = {'Name': 'Betsy Foobar',
+              'Phone': 'x8012',
+              'Street': '1600 Grand Avenue',
+              'City': 'Saint Paul',
+              'State': 'MN',
+              'Email': 'bfoobar@macalester.edu'}
+
+tom_info = {'Name': 'Tom Riddle',
+            'Phone': 'x8512',
+            'Street': '1600 Grand Avenue',
+            'City': 'Saint Paul',
+            'State': 'MN',
+            'Email': 'triddle@macalester.edu'}
+address_book = [ betsy_info, tom_info,
+                {'Name': 'Susan Fox',
+                 'Phone': 'x6553',
+                 'Street': '1600 Grand Avenue',
+                 'City': 'Saint Paul',
+                 'State': 'MN',
+                 'Email': 'fox@macalester.edu'},
+                 {'Name': 'Apiu',
+                  'Phone':'802302',
+                  'Street':'345 Main Street',
+                  'City':'Minneapolis',
+                  'State':'MN',
+                  'Email':'apiu@gmail.com'},
+                 {'Name':'Helen',
+                  'Phone':'54823975',
+                  'Street':'2500 College Street',
+                  'City':'San Francisco',
+                  'State':'CA',
+                  'Email':'hlevy@gmail.com'}]
+print(address_book)
+
+def filter_by_city(city_name,address_book):
+    newlist=[]
+    for address in address_book:
+        if address['City'] == city_name:
+            newlist.append(address)
+    return newlist
+
+print(filter_by_city('Saint Paul',address_book))
