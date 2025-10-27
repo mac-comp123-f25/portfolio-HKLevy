769a9c4b98ca92445a5f228bc92872fff87b56f4
HKLevy <hlevy@macalester.edu>
2025-10-26 20:17:36 -0500

    Update solo_project.py

--- src/sp/solo_project.py
+++ src/sp/solo_project.py
@@ -1,11 +1,18 @@
 import turtle
 win = turtle.Screen()
 turt = turtle.Turtle()
+turt.speed(0)
+turt.ht()
 import math
 
 from polar_coordinate_graphing import graph_sin_theta_transformation
 
 if __name__ == '__main__':
+    """
+    This is starting off by looking very nice. Just maybe not what I want, but I also don't want to delete it.
+    I found an inspiration picture that I think I can mimic/adapt.
+    This will challenge me more in terms of creativity and coding.
+    
     graph_sin_theta_transformation(turt,'black',0,180,0,0,50,3,0)
     graph_sin_theta_transformation(turt,'black',0,180,0,0,50,3,180)
     turt.up()
@@ -35,4 +42,23 @@ if __name__ == '__main__':
     turt.seth(0)
     turt.down()
     turt.circle(50+2*new_radius)
+    """
+
+    """Take user input in terms of number of layers and color and size."""
+    layers = 3 #will become user inputted later.
+    start_size = 75
+    inc_size = 50
+    for x in range(layers):
+        petal_size = start_size + x * inc_size
+        graph_sin_theta_transformation(turt,'black',0,180,0,0,petal_size,3,30)
+        graph_sin_theta_transformation(turt,'black',0,180,0,0,petal_size,3,90)
+        turt.up()
+        for y in range(6):
+            turt.fd(petal_size)
+            turt.dot(15)
+            turt.bk(petal_size)
+            turt.rt(60)
+        turt.down()
+    turt.dot(35)
+
     win.exitonclick()
