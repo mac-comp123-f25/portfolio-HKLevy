f57e7876fa873b6c05d4985e3f17888329b6a816
HKLevy <hlevy@macalester.edu>
2025-11-02 22:27:05 -0600

    cleaned up code, added a little

--- src/sp/solo_project.py
+++ src/sp/solo_project.py
@@ -12,7 +12,16 @@ def line(t,length):
     t.fd(length)
     t.bk(length)
 
-#implement line function elsewhere. use it in a separator function that takes inner radius, separator length and num of separations
+def separator(t,inside_radius,thickness,num_separators):
+    t.up()
+    t.goto(0,-1*(inside_radius+thickness))
+    t.seth(0)
+    t.down()
+    for a in range(num_separators):
+        t.lt(90)
+        line(t,thickness)
+        t.rt(90)
+        t.circle(inside_radius+thickness,extent=int(360/num_separators))
 
 if __name__ == '__main__':
     """
@@ -25,8 +34,9 @@ if __name__ == '__main__':
     """
 
     """Will have to do a thing if they give me a letter that dne here"""
-    color_dict={'R':None,'O':None,'Y':None,'G':None,'B':None,'V':shading('#f0c4ff','#bb02fa',7),'P':None}
+    color_dict={'R':None,'O':None,'Y':None,'G':None,'B':None,'V':shading('#f0c4ff','#bb02fa',8),'P':None}
     color_list=color_dict['V']
+    print(color_list)
 
     radius1 = 50 #may respond to user later.
 
@@ -39,21 +49,20 @@ if __name__ == '__main__':
     graph_sin_theta_transformation(turt,color_list[0],0,180,0,0,radius1,3,180)
     turt.end_fill()
 
-    #draws the circle with radius1
+    #draws the circle around the 6 petals with radius1
     turt.up()
-    lstpts12=[]
     turt.goto(0,-1*radius1)
     turt.down()
-    for x in range(12):
-        lstpts12.append(turt.pos())
-        turt.circle(radius1,extent=30)
+    turt.circle(radius1)
 
     #draws the 12 dividing lines through the center
-    for x in range(1,len(lstpts12),2):
-        turt.up()
-        turt.goto(lstpts12[x])
-        turt.down()
-        turt.goto(0,0)
+    turt.up()
+    turt.goto(0,0)
+    turt.seth(0)
+    turt.down()
+    for x in range(6):
+        line(turt,50)
+        turt.lt(60)
 
     #draws the 12 small circles each with radius small_circ_radius and shades around them
     small_circ_radius = radius1*math.sin(math.radians(15))/(1-math.sin(math.radians(15)))
@@ -90,29 +99,14 @@ if __name__ == '__main__':
         #moves to next circle spot
         turt.circle(radius2,extent=30)
 
-    #draws a circle of radius2 around the 12 small circles
-    turt.up()
-    turt.goto(0,-1*(radius2))
-    turt.seth(0)
-    turt.down()
-    turt.circle(radius2)
-
     #draws a separator around what we have so far
-    radius3 = radius2+15
-    turt.up()
-    turt.goto(0,-1*(radius3))
-    turt.down()
     turt.color(color_list[2])
-    for x in range(60):
-        turt.lt(90)
-        turt.fd(15)
-        turt.bk(15)
-        turt.rt(90)
-        turt.circle(radius3,extent=6)
+    separator(turt,radius2,15,60)
+    radius3 = radius2 + 15
 
     #draws 6 partial circles coming off of the central figure
     turt.color(color_list[4])
-    circ240_radius = (radius3)/math.sqrt(3)
+    circ240_radius = radius3/math.sqrt(3)
     for x in range(6):
         turt.rt(90)
         turt.circle(circ240_radius,extent=240)
@@ -120,34 +114,23 @@ if __name__ == '__main__':
 
     #draws 6 petals inside each of the partial circles
     circ240_dist = circ240_radius*2
-    turt.pensize(5)
     for x in range(6):
         big_angle=60*x
         turt.up()
-        turt.goto(0,0)
-        turt.seth(0)
-        turt.lt(big_angle)
-        turt.fd(circ240_dist)
-        center=turt.pos()
-
+        center=(circ240_dist*math.cos(math.radians(big_angle)),circ240_dist*math.sin(math.radians(big_angle)))
+        turt.goto(center)
         turt.down()
-        #turt.color(color_list[3])
         for y in range(6):
-            #small_angle = -10 + 40 * y
-            #small_angle = -13 + 41 * y closer but not tangent
-            small_angle = -16 + 42 * y # I like that a lot :) keeping others for reference.
-            graph_sin_theta_transformation(turt,color_list[3],0,60,center[0],center[1],circ240_radius,3,big_angle+small_angle-120)
-    turt.pensize(2)
-    #draws a circle around everything we have so far
-    radius4 = circ240_dist + circ240_radius
-    turt.up()
-    turt.goto(0,-1*radius4)
-    turt.seth(0)
-    turt.down()
-    turt.color(color_list[4])
-    turt.circle(radius4,extent=30)
+            small_angle = -16 + 42 * y #gotten through trial and error
+            #graph_sin_theta_transformation(turt,color_list[3],0,60,center[0],center[1],circ240_radius,3,big_angle+small_angle-120)
 
     #shades in the areas around the 6 partial circles that is inside the largest circle
+    radius4 = circ240_dist + circ240_radius
+    turt.up()
+    turt.goto(radius4,0)
+    turt.lt(90)
+    turt.down()
+    turt.color(color_list[4])
     for x in range(6):
         turt.begin_fill()
         turt.circle(circ240_radius,extent=120)
@@ -158,38 +141,44 @@ if __name__ == '__main__':
         turt.circle(radius4,extent=60)
 
     #draws a separator around everything we have so far.
+    separator(turt,radius4,25,60)
     radius5 = radius4 + 25
-    turt.up()
-    turt.rt(90)
-    turt.fd(25)
-    turt.lt(90)
-    turt.down()
-    turt.color(color_list[5])
-    for x in range(60):
-        turt.circle(radius5,extent=6)
-        turt.lt(90)
-        turt.fd(25)
-        turt.bk(25)
-        turt.rt(90)
-    turt.pensize(3)
-    turt.circle(radius5)
 
-    #draws little firework thingies at the topmost part of each partial circle
+    #draws 30 tiny circles around what we have so far.
+    tiny_radius = radius5*math.sin(math.radians(6))/(1-math.sin(math.radians(6)))
     turt.color(color_list[6])
-    turt.pensize(5)
-    for x in range(60):
-        if x%10==0:
-            turt.rt(90)
-            for y in range(3):
-                line(turt,35-10*y)
-                turt.rt(30)
-            turt.lt(120)
-            for y in range(1,3):
-                line(turt,35-10*y)
-                turt.lt(30)
-        turt.up()
-        turt.circle(radius5, extent=6)
-        turt.down()
+    for x in range(30):
+        turt.rt(180)
+        if x%5==0:
+            turt.begin_fill()
+            turt.circle(tiny_radius)
+            turt.end_fill()
+        else:
+            turt.circle(tiny_radius)
+        turt.rt(180)
+        turt.circle(radius5,extent=12)
+
+    # radius6 = radius5 + 2 * tiny_radius
+    # turt.up()
+    # turt.goto(radius6,0)
+    # turt.down()
+    # turt.lt(90)
+    # turt.circle(radius6)
+    # #draws little firework thingies at the topmost part of each partial circle. save for end?
+    # turt.color(color_list[7])
+    # turt.pensize(5)
+    # for x in range(6):
+    #     turt.rt(90)
+    #     for y in range(3):
+    #         line(turt,35-10*y)
+    #         turt.rt(30)
+    #     turt.lt(120)
+    #     for y in range(1,3):
+    #         line(turt,35-10*y)
+    #         turt.lt(30)
+    #     turt.up()
+    #     turt.circle(radius6, extent=60)
+    #     turt.down()
 
     """Take user input in terms of number of layers and color and size.
     layers = 5 #will become user inputted later.
@@ -238,6 +227,7 @@ if __name__ == '__main__':
             graph_sin_theta_transformation(turt,'black',0,11,0,0,260,5,angle-18)
             
     Still experimenting with petal sizes for inside and outside poofs"""
+    print("End state: ",turt.pos(),turt.heading())
     win.exitonclick()
 
 
