c4d33af2aedbc755faaf05cb413a2ec340639d54
HKLevy <hlevy@macalester.edu>
2025-11-10 10:42:07 -0600

    finished ica with apiu

--- src/ica/15-imageTools/manipulate_color.py
+++ src/ica/15-imageTools/manipulate_color.py
@@ -14,16 +14,31 @@ def change_blue(picture,factor):
         new_blue = blue*factor
         picture.setColor(x,y,(red,green,new_blue))
 
+def remove_blue(picture):
+    for (x,y) in picture:
+        (r,g,b)=picture.getColor(x,y)
+        picture.setColor(x,y,(r,g,0))
+
+def fix_green(picture,num):
+    for (x,y) in picture:
+        (r,g,b)=picture.getColor(x,y)
+        picture.setColor(x,y,(r,num,b))
+
 def main():
     # fruit_pic = Picture("../SampleImages/raspberries.jpg")
     # fruit_pic.show()
     # change_red(fruit_pic, 1.2)
     # fruit_pic.show()
 
-    blue_pic = Picture("../SampleImages/fireworks.jpg")
-    blue_pic.show()
-    change_blue(blue_pic,4.0)
-    blue_pic.show()
+    # blue_pic = Picture("../SampleImages/fireworks.jpg")
+    # blue_pic.show()
+    # change_blue(blue_pic,4.0)
+    # blue_pic.show()
+
+    new_pic = Picture("../SampleImages/canyonlands.jpg")
+    new_pic.show()
+    fix_green(new_pic,100)
+    new_pic.show()
 
     keep_windows_open()
 
