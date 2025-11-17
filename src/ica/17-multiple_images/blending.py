094dbf6c4b4f3a97d4151b79518afb1ec42d6130
HKLevy <hlevy@macalester.edu>
2025-11-17 10:11:33 -0600

    blend2

--- src/ica/17-multiple_images/blending.py
+++ src/ica/17-multiple_images/blending.py
@@ -13,9 +13,29 @@ def simple_blend(pic1,pic2):
 
     return new_pic
 
-p1=Picture("../SampleImages/daylilies.jpg")
-p2=Picture("../SampleImages/wildColumbine.jpg")
-p3=simple_blend(p1,p2)
-p3.show()
+def blend2(pic1,pic2):
+    w = min(pic1.getWidth(),pic2.getWidth())
+    h = min(pic1.getHeight(),pic2.getHeight())
+    new_pic=Picture(w,h)
+
+    for (x,y) in new_pic:
+        (r1,g1,b1)=pic1.getColor(x,y)
+        (r2,g2,b2)=pic2.getColor(x,y)
+        (r,g,b)=(r1+r2)/2, (g1+g2)/2, (b1+b2)/2
+        new_pic.setColor(x,y,(r,g,b))
+        if y==pic1.getHeight():
+            new_pic.show()
+
+    return new_pic
+
+# p1=Picture("../SampleImages/daylilies.jpg")
+# p2=Picture("../SampleImages/wildColumbine.jpg")
+# p3=simple_blend(p1,p2)
+# p3.show()
+
+p4 = Picture("../SampleImages/muirWoods.jpg")
+p5 = Picture("../SampleImages/peony.jpg")
+p6 = blend2(p4,p5)
+p6.show()
 
 keep_windows_open()
