0a4035b873543663e1630265dde6534500507182
HKLevy <hlevy@macalester.edu>
2025-11-26 09:48:32 -0500

    added, alphabetized, and used stuff

--- src/Practice Problems/crossword_helper.py
+++ src/Practice Problems/crossword_helper.py
@@ -1,23 +1,3 @@
-def find_words(str):
-    """Takes in a string with some blank spots represented by *.
-    Returns all words acceptable in crosswords that follow that pattern."""
-    word_file = open("../ica/TextFiles/crosswords.txt")
-    word_length = len(str)
-    word_list=[]
-    for line in word_file:
-        condition = True
-        if len(line) == word_length+1: #+1 because there is the \n character
-            for c in range(word_length):
-                if str[c]!=line[c] and str[c]!='*':
-                    condition = False
-            if condition == True:
-                word_list.append(line)
-    return word_list
-
-# print(find_words('see****'))
-# print(find_words('e**o'))
-
-
 def find_words_with(str):
     """Takes in a string, may include some blank spots represented by *.
     Returns all acceptable answers that have that string in any place inside them."""
@@ -28,17 +8,40 @@ def find_words_with(str):
             word_list.append(line)
     return word_list
 
-def find_vowels(str):
-    """Takes in a string made only of C or V.
-    Returns all acceptable answers that have that consonant/vowel pattern."""
-    vowel_list=['a','e','i','o','u']
-    word_file = open("../ica/TextFiles/crosswords.txt")
+def find_words_in_file(big_file,key):
+    words=open(big_file)
     word_list=[]
-    word_length=len(str)
-    for line in word_file:
+    length=len(key)
+    for line in words:
+        #This will work by toggling the condition to false if something is wrong.
+        #That way, if any part is false, condition=False. condition can never be switched back to True
         condition = True
-        if len(line)==word_length+1:
-            for c in range(word_length)
+        if len(line) == length+1:
+            for letter in range(length):
+                if key[letter]=='V' and line[letter] not in ['a','e','i','o','u','y']:
+                    #If a vowel is requested and it's not a vowel...
+                    condition = False
+                elif key[letter]=='C' and line[letter] in ['a','e','i','o','u']:
+                    #If a consonant is requested and it is a vowel...
+                    condition = False
+                elif key[letter]!='*' and key[letter]!='V' and key[letter]!='C' and key[letter]!=line[letter]:
+                    #If it's a specific letter but it doesn't match...
+                    condition = False
 
+            if condition:
+                #If every letter matched what we gave it...
+                word_list.append(line)
 
     return word_list
+
+
+def find_words(str):
+    """Accepts *, C,V, or specific lowercase letters.
+    Should consider acronyms and normal words. Add names later?"""
+    word_list = find_words_in_file('better_crosswords.txt',str)
+    word_list = word_list + find_words_in_file('crossword_acronyms_and_other.txt',str)
+
+    word_list.sort()
+    return word_list
+
+print(find_words('iot**'))
