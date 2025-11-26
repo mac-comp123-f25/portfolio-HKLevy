17b9b13c9706ddcaaa85c71e9f933ab9b825b0fe
HKLevy <hlevy@macalester.edu>
2025-11-25 22:40:22 -0500

    added things for fun

--- src/Practice Problems/crossword_helper.py
+++ src/Practice Problems/crossword_helper.py
@@ -14,5 +14,31 @@ def find_words(str):
                 word_list.append(line)
     return word_list
 
-print(find_words('see****'))
-print(find_words('e**o'))
+# print(find_words('see****'))
+# print(find_words('e**o'))
+
+
+def find_words_with(str):
+    """Takes in a string, may include some blank spots represented by *.
+    Returns all acceptable answers that have that string in any place inside them."""
+    word_file = open("../ica/TextFiles/crosswords.txt")
+    word_list=[]
+    for line in word_file:
+        if str in line:
+            word_list.append(line)
+    return word_list
+
+def find_vowels(str):
+    """Takes in a string made only of C or V.
+    Returns all acceptable answers that have that consonant/vowel pattern."""
+    vowel_list=['a','e','i','o','u']
+    word_file = open("../ica/TextFiles/crosswords.txt")
+    word_list=[]
+    word_length=len(str)
+    for line in word_file:
+        condition = True
+        if len(line)==word_length+1:
+            for c in range(word_length)
+
+
+    return word_list
